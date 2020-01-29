1006c24fbf2ea43d91b729b2f10480e1df7f0019
Cody Boaz <codyryanboaz@yahoo.com>
2020-01-28 21:21:23 -0800

    set up initial api call and receiving data

--- app/components/Articles.js
+++ app/components/Articles.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { getTopPosts } from '../utils/api'
 
 export default class Articles extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      loading: true,
+      stories: null
+    }
+  }
+  componentDidMount() {
+    getTopPosts()
+      .then((stories) => {
+        console.log(stories)
+        this.setState({
+          loading: false,
+          stories
+        })
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   }
   render() {
+    if (this.state.loading) {
+      return <p>Loading</p>
+    }
     return (
-      <h1>These are the Articles!</h1>
+      <ul>
+        {this.state.stories.map((story) => (
+          <li key={story.id}>
+            {story.title}
+          </li>
+        ))}
+      </ul>
     )
   }
 }
---
+++ app/utils/api.js
@@ -0,0 +1,25 @@
+export function getTopPosts() {
+  return fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+    .then((data) => data.json())
+    .then((ids) => {
+      if (!ids) {
+        throw new Error('There was an error fetching the posts')
+      }
+      return ids.slice(0, 50)
+    })
+    .then((ids) => Promise.all(ids.map(getStoryFromId)))
+    .then((posts) => removeDeleted(removeDead(posts)))
+}
+
+function getStoryFromId(id) {
+  return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+    .then((data) => data.json())
+}
+
+function removeDead(posts) {
+  return posts.filter(({ dead }) => dead !== true)
+}
+
+function removeDeleted(posts) {
+  return posts.filter(({ deleted }) => deleted !== true)
+}
